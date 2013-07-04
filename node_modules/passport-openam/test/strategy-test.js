38538f472cf86b890095f1c67b07091d8765666f
adamstacey5 <astacey_1991@hotmail.com>
2013-07-04 13:56:16 +0100

    Removing Node_Modules

--- node_modules/passport-openam/test/strategy-test.js
+++
@@ -1,6 +0,0 @@
-var vows = require('vows');
-var assert = require('assert');
-var util = require('util');
-var OpenAmStrategy = require('passport-openam/strategy');
-
-vows.describe('OpenAmStrategy').addBatch({});
